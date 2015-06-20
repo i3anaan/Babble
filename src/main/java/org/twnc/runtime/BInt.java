94d2329aaee24eacca46e231f4bbed1cf8bd328c
Wander Nauta <info@wandernauta.nl>
2015-06-20 20:09:59 +0200

    Reformat BInt a bit

--- src/main/java/org/twnc/runtime/BInt.java
+++ src/main/java/org/twnc/runtime/BInt.java
@@ -19,83 +19,49 @@ public class BInt extends BObject {
     }
 
     public BInt(BInt i) {
-        integer = i.getInteger();
+        integer = i.integer;
     }
 
     private static BObject op(BObject a, BObject b, BinaryOperator<BigInteger> lambda) {
-        BigInteger ai = ((BInt) a._asInt()).getInteger();
-        BigInteger bi = ((BInt) b._asInt()).getInteger();
+        BigInteger ai = ((BInt) a._asInt()).integer;
+        BigInteger bi = ((BInt) b._asInt()).integer;
         return new BInt(lambda.apply(ai, bi));
     }
 
     private static int cmp(BObject a, BObject b) {
-        BigInteger ai = ((BInt) a._asInt()).getInteger();
-        BigInteger bi = ((BInt) b._asInt()).getInteger();
+        BigInteger ai = ((BInt) a._asInt()).integer;
+        BigInteger bi = ((BInt) b._asInt()).integer;
         return ai.compareTo(bi);
     }
 
-    public BObject _plus_(BObject that) {
-        return op(this, that, BigInteger::add);
-    }
+    public BObject _plus_(BObject that)   { return op(this, that, BigInteger::add); }
+    public BObject _minus_(BObject that)  { return op(this, that, BigInteger::subtract); }
+    public BObject _star_(BObject that)   { return op(this, that, BigInteger::multiply); }
+    public BObject _slash_(BObject that)  { return op(this, that, BigInteger::divide); }
+    public BObject _mod_(BObject that)    { return op(this, that, BigInteger::mod); }
 
-    public BObject _minus_(BObject that) {
-        return op(this, that, BigInteger::subtract);
-    }
-
-    public BObject _star_(BObject that) {
-        return op(this, that, BigInteger::multiply);
-    }
-
-    public BObject _slash_(BObject that) {
-        return op(this, that, BigInteger::divide);
-    }
-
-    public BObject _mod_(BObject that) {
-        return op(this, that, BigInteger::mod);
-    }
-
-    public BObject _lt_(BObject that) {
-        return BBool.of(cmp(this, that) < 0);
-    }
-
-    public BObject _eqeq_(BObject that) {
-        return BBool.of(cmp(this, that) == 0);
-    }
-
-    public BObject _gt_(BObject that) {
-        return BBool.of(cmp(this, that) > 0);
-    }
-
-    public BObject _gteq_(BObject that) {
-        return BBool.of(cmp(this, that) >= 0);
-    }
-
-    public BObject _bangeq_(BObject that) {
-        return BBool.of(cmp(this, that) != 0);
-    }
-
-    public BObject _lteq_(BObject that) {
-        return BBool.of(cmp(this, that) <= 0);
-    }
+    public BObject _lt_(BObject that)     { return BBool.of(cmp(this, that) < 0); }
+    public BObject _eqeq_(BObject that)   { return BBool.of(cmp(this, that) == 0); }
+    public BObject _gt_(BObject that)     { return BBool.of(cmp(this, that) > 0); }
+    public BObject _gteq_(BObject that)   { return BBool.of(cmp(this, that) >= 0); }
+    public BObject _bangeq_(BObject that) { return BBool.of(cmp(this, that) != 0); }
+    public BObject _lteq_(BObject that)   { return BBool.of(cmp(this, that) <= 0); }
 
     public BObject _negate() {
-        return new BInt(this.getInteger().negate());
+        return new BInt(integer.negate());
     }
 
     public BObject _abs() {
-        return new BInt(this.getInteger().abs());
+        return new BInt(integer.abs());
     }
 
     public BObject _asBool() {
-        return BBool.of(!this.getInteger().equals(BigInteger.ZERO));
+        return BBool.of(!integer.equals(BigInteger.ZERO));
     }
 
+    @Override
     public BInt _asInt() {
         return new BInt(this);
-    }
-
-    private BigInteger getInteger() {
-        return integer;
     }
 
     @Override
@@ -105,11 +71,11 @@ public class BInt extends BObject {
 
     @Override
     public boolean equals(Object that) {
-        return that instanceof BInt && ((BInt)that).getInteger().equals(this.getInteger());
+        return that instanceof BInt && ((BInt)that).integer.equals(integer);
     }
 
     @Override
     public int hashCode() {
-        return this.getInteger().hashCode();
+        return integer.hashCode();
     }
 }
