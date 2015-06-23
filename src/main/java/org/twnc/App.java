4186c28a4f4660bccc2aa2ff85b6c3e9c97ada92
Wander Nauta <info@wandernauta.nl>
2015-06-23 12:46:49 +0200

    Simplify integration tests by removing Failsafe
    This drops the maven/Failsafe thing and instead has the test program call the
    noisy assert function in its main function. `mvn verify` now just compiles and
    runs the test program. If an assertion fails, an exception is thrown, which
    Maven picks up as 'build failed'.

--- src/main/java/org/twnc/App.java
+++ src/main/java/org/twnc/App.java
@@ -75,17 +75,6 @@ public class App extends BabbleBaseListener implements Opcodes {
         {
             mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
             mv.visitCode();
-            mv.visitTypeInsn(NEW, name);
-            mv.visitInsn(DUP);
-            mv.visitMethodInsn(INVOKESPECIAL, name, "<init>", "()V", false);
-            mv.visitMethodInsn(INVOKEVIRTUAL, name, "testAll", "()V", false);
-            mv.visitInsn(RETURN);
-            mv.visitMaxs(0, 0);
-            mv.visitEnd();
-        }
-        {
-            mv = cw.visitMethod(ACC_PUBLIC, "testAll", "()V", null, null);
-            mv.visitCode();
         }
     }
 
